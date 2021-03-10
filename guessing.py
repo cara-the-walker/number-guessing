489b8606668a4472477e76c06ea92bfe210b4ace
“Cara <cara1pig@yahoo.com.hk>
2021-03-10 22:55:35 +0800

    Docs.: Update guess() to tell proper input

--- guessing.py
+++ guessing.py
@@ -5,21 +5,32 @@ import random
 #def display(input):
 #    print('The answer is ',answer,'!')
 
+
+
+
 def guess(ans):
 
     dic = {'target' : ans}
     r   = range(1,100)
 
     while True:
+        while True:
 
-        guess = int(input('Please make you guess by entering an integral number between 0 and 100: ').strip())
-        print(type(guess),'  ',guess,'  ',dic['target'], '  ',isinstance(ans,int), '  ',guess in r)
+            guess = input('Please make you guess by entering an integral number between 0 and 100: ').strip()
 
-        if isinstance(guess,int) and guess in r:
+            try:
+                int(guess)
+                break
+            except ValueError:
+                print('\n\nYour input is not recoginizable. \nWe\'ll need to ask you for ONLY integral number.\n\n')
+
+        guess = int(guess)
+    #    print(type(guess),'  ',guess,'  ',dic['target'], '  ',isinstance(ans,int), '  ',guess in r)
+
+        if guess in r:
             dic['user input'] = guess
-            break
-
-        print('Your input is not recoginizable. We\'ll need to ask you for another input. ')
+            break;
+        else: print('\n\nYour input is not recoginizable. \nWe\'ll need to ask you for that number BETWEEN 0 AND 100.\n\n')
 
     return dic
 
@@ -28,7 +39,8 @@ def main():
     print('Welcome to Number Guessing game!! \n\nThe target number is between 0 and 100.\n\n')
     answer = int(random.random()*100)
     while True:
-        guess(answer)
+        num = guess(answer)
+        print(num)
 #        print(guess(answer))
         print('break')
         break
